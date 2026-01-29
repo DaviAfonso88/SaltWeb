fada35697accd7ab714b2439d86a7a9dfeeffbc4
Daviafonso88 <daviafonso5894@gmail.com>
2026-01-29 09:29:53 -0300

    Remova os detalhes desatualizados do evento e atualize a data do evento "Culto Salt" nos dados de eventos

--- app/eventos/data.ts
+++ app/eventos/data.ts
@@ -10,23 +10,13 @@ export type Evento = {
 
 export const eventos: Evento[] = [
   {
-    titulo: "Projeto Missionário da JUBAM",
-    data: "16",
-    mes: "JAN",
-    imagem: "/images/evento01.jpg",
-    link: "#",
-    description:
-      "Participe do projeto missionário anual da JUBAM. Um tempo de serviço, aprendizado e transformação de vidas em comunidades carentes. Inscrições abertas!",
-    location: "Igreja Batista Central",
-  },
-  {
     titulo: "Culto Salt",
-    data: "30",
+    data: "31",
     mes: "JAN",
     imagem: "/images/culto-start.png",
     link: "#",
     description:
       "Venha viver o primeiro culto da SALT do ano, um momento especial de celebração com a posse dos novos líderes da juventude.",
     location: "Primeira Igreja Batista em Lagoa Santa",
-  },
+  }
 ];
