5f7b0bd40b2c00d9298a2c13bc2aaffcee793d2a
Daviafonso88 <daviafonso5894@gmail.com>
2026-03-31 22:56:07 -0300

    Adiciona novos eventos "Culto Salt" e "Cantata de Páscoa" à lista de eventos, enriquecendo a programação com descrições, datas, imagens e informações de localização.

--- app/eventos/data.ts
+++ app/eventos/data.ts
@@ -36,4 +36,26 @@ export const eventos: Evento[] = [
     link: "/acampa-salt",
     linkLabel: "Fazer inscricao",
   },
+  {
+    id: "culto-salt-2026-04-03",
+    titulo: "Culto Salt",
+    description:
+      "Crises de fé fazem parte da caminhada, mas não definem o nosso fim. Neste culto, vamos entender como Deus usa até os momentos difíceis para fortalecer nossa fé.",
+    dateISO: "2026-04-03T19:30:00-03:00",
+    imagens: ["/images/culto-salt-abril.png"],
+    tags: ["Culto", "Juventude"],
+    locationName: "R. Jair Gonçalves Bastos, 64 - Jardim Ipê, Lagoa Santa",
+    mapsUrl: "https://maps.app.goo.gl/DDxiX65jtCC47ETF7",
+  },
+  {
+    id: "cantata-pascoa-2026-04-05",
+    titulo: "Cantata de Páscoa",
+    description:
+      "Uma dia especial para celebrar a esperança e o amor revelados na cruz. Na cantata de Páscoa, vamos relembrar o sacrifício e a vitória de Jesus por nós.",
+    dateISO: "2026-04-05T10:00:00-03:00",
+    imagens: ["/images/cantata-de-pascoa.png"],
+    tags: ["Cantata", "Páscoa"],
+    locationName: "R. Jair Gonçalves Bastos, 64 - Jardim Ipê, Lagoa Santa",
+    mapsUrl: "https://maps.app.goo.gl/DDxiX65jtCC47ETF7",
+  },
 ];
